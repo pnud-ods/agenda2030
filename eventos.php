8478aed224118b6d670a5e9d470ffe069e86a589
matpamoreira <matpamoreira@gmail.com>
2016-10-14 00:26:31 -0300

    Consulta dos eventos no banco de dados.

--- eventos.php
+++ eventos.php
@@ -1,8 +1,37 @@
 <?php
 include_once 'partes.php';
-
 getHeader();
 ?>
+    <script type="text/javascript">
+        var data = new Date();
+        function carregaMesAnt() {
+            data.setMonth(data.getMonth() - 1)
+            carregaMes();
+        }
+        function carregaMesProx(){
+            data.setMonth(data.getMonth() + 1)
+            carregaMes();
+        }
+        function carregaMes(){
+            var eventos = $('#eventos');
+            eventos.empty();
+            var calendario = $('#calendario');
+            calendario.empty();
+            $.ajax({
+                url: 'get_eventos.php?ano=' + data.getFullYear() + '&mes=' + (data.getMonth() + 1),
+                dataType: 'json'
+            }).done(function(retorno){
+                eventos.append(retorno.eventos);
+                calendario.append(retorno.calendario);
+                $('#num_ano').html(retorno.ano);
+                $('#nom_mes').html(retorno.mes);
+            });
+        }
+
+        $( document ).ready(function(){
+            carregaMes();
+        })
+    </script>
     <link rel="stylesheet" href="css/agenda.css">
     <div class="row" style="background-color:#eeeeee">
         <div class="col-xs-10 col-xs-offset-1">
@@ -13,11 +42,11 @@ getHeader();
                     <div class="col-xs-8 left-side-calendar-container">
                         <div class="month">
                             <ul>
-                                <li class="prev">&#10094;</li>
-                                <li class="next">&#10095;</li>
+                                <li class="prev"><a title="Exibir mês anterior" onclick="carregaMesAnt();">&#10094;</a></li>
+                                <li class="next"><a title="Exibir próximo mês" onclick="carregaMesProx();">&#10095;</a></li>
                                 <li class="text-center" style="font-weight: bold;">
-                                    Outubro<br>
-                                    <span style="font-size:18px">2016</span>
+                                    <div id="nom_mes"></div>
+                                    <span id="num_ano" style="font-size:18px"></span>
                                 </li>
                             </ul>
                         </div>
@@ -31,92 +60,20 @@ getHeader();
                             <li>Sab</li>
                             <li>Dom</li>
                         </ul>
-
-                        <ul class="days">
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li></li>
-                            <li>1</li>
-                            <li>2</li>
-                            <li>3</li>
-                            <li>4</li>
-                            <li>5</li>
-                            <li>6</li>
-                            <li>7</li>
-                            <li>8</li>
-                            <li>9</li>
-                            <li><span class="active">10</span></li>
-                            <li>11</li>
-                            <li>12</li>
-                            <li>13</li>
-                            <li>14</li>
-                            <li>15</li>
-                            <li>16</li>
-                            <li>17</li>
-                            <li>18</li>
-                            <li>19</li>
-                            <li>20</li>
-                            <li>21</li>
-                            <li>22</li>
-                            <li>23</li>
-                            <li>24</li>
-                            <li>25</li>
-                            <li><span class="active">26</span></li>
-                            <li>27</li>
-                            <li>28</li>
-                            <li>29</li>
-                            <li><span class="active">30</span></li>
-                            <li>31</li>
+                        <ul id="calendario" class="days">
                         </ul>
-
                     </div>
                     <div class="col-xs-4" style="background-color:#264c5f;border-radius:0 10px 10px 0; margin-bottom:0px;height:550px;">
-
-                        <div class="row" style="margin-top:70px;">
+                        <div class="row" style="margin-top:30px;">
                             <div class="col-xs-12 text-center">
-											<span class="title-font" style="font-size:16pt;color:#ff9d1e;font-weight:bold;">
-												Agenda de Eventos
-											</span>
+                                <span class="title-font" style="font-size:16pt;color:#ff9d1e;font-weight:bold;">Agenda de Eventos</span>
                                 <div class="border-botton-events-list"></div>
                             </div>
                         </div>
-
-                        <div class="row" style="margin-top:105px;">
-                            <div class="col-xs-8 col-xs-offset-2 text-center">
-											<span class="agenda-events-label">
-												CONFERÊNCIA INTERNACIONAL DOS ODS
-											</span>
-                                <div class="border-botton-events-list"></div>
-                            </div>
-                        </div>
-
-
-                        <div class="row"  style="margin-top:55px;">
-                            <div class="col-xs-8 col-xs-offset-2 text-center">
-											<span class="agenda-events-label">
-												CONGRESSO DA ONU
-											</span>
-                                <div class="border-botton-events-list"></div>
-                            </div>
-                        </div>
-
-                        <div class="row"  style="margin-top:55px;">
-                            <div class="col-xs-8 col-xs-offset-2 text-center">
-											<span class="agenda-events-label">
-												CIDADES VERDES
-											</span>
-                                <div class="border-botton-events-list"></div>
-                            </div>
-                        </div>
-
-
+                        <div id="eventos"></div>
                     </div>
                 </div>
             </div>
-            <!-- ./calendar container -->
-
         </div>
     </div>
 <?php
---
+++ get_eventos.php
@@ -0,0 +1,84 @@
+<?php
+include_once 'conectar.php';
+$ano = $_REQUEST['ano'];
+$mes = $_REQUEST['mes'];
+$retorno = array();
+$sql = "select day(dat_inicio) as dia, e.dat_inicio, e.nom_evento from evento e
+         where year(e.dat_inicio) = $ano
+           and month(e.dat_inicio) = $mes
+         order by e.dat_inicio";
+$result = $conn->query($sql);
+$retorno['eventos'] = '';
+
+$retorno['calendario'] = '';
+$primeiro_dia_semana = date("w", strtotime("$ano/$mes/01") ) - 1;
+for($i = 1; $i <= $primeiro_dia_semana; $i++){
+    $retorno['calendario'] .= "<li></li>";
+}
+
+$quant_dias = cal_days_in_month(CAL_GREGORIAN, $mes, $ano);
+$row = $result->fetch_assoc();
+for($i = 1; $i <= $quant_dias; $i++){
+    if($row['dia'] != $i){
+        $retorno['calendario'] .= "<li>$i</li>";
+    }
+    else{
+        $retorno['calendario'] .= "<li><span class=\"active\">$i</span></li>";
+        while($row['dia'] == $i){
+            $retorno['eventos'] .= '<div class="row" style="margin-top:50px;">';
+            $retorno['eventos'] .= '<div class="col-xs-8 col-xs-offset-2 text-center">';
+            $retorno['eventos'] .= '<span class="agenda-events-label">';
+            $retorno['eventos'] .= $row['nom_evento'];
+            $retorno['eventos'] .= '</span>';
+            $retorno['eventos'] .= '<div class="border-botton-events-list"></div>';
+            $retorno['eventos'] .= '</div>';
+            $retorno['eventos'] .= '</div>';
+            $row = $result->fetch_assoc();
+        }
+    }
+}
+$retorno['ano'] = $ano;
+switch($mes){
+    case 1:
+        $retorno['mes'] = 'Janeiro';
+        break;
+    case 2:
+        $retorno['mes'] = 'Fevereiro';
+        break;
+    case 3:
+        $retorno['mes'] = 'Março';
+        break;
+    case 4:
+        $retorno['mes'] = 'Abril';
+        break;
+    case 5:
+        $retorno['mes'] = 'Maio';
+        break;
+    case 6:
+        $retorno['mes'] = 'Junho';
+        break;
+    case 7:
+        $retorno['mes'] = 'Julho';
+        break;
+    case 8:
+        $retorno['mes'] = 'Agosto';
+        break;
+    case 9:
+        $retorno['mes'] = 'Setembro';
+        break;
+    case 10:
+        $retorno['mes'] = 'Outubro';
+        break;
+    case 11:
+        $retorno['mes'] = 'Novembro';
+        break;
+    case 12:
+        $retorno['mes'] = 'Dezembro';
+        break;
+    default:
+        $retorno['mes'] = 'XXX';
+}
+
+$result->free();
+die( json_encode($retorno, JSON_UNESCAPED_UNICODE) );
+
