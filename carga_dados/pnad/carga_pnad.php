fd7917b5f49641f374691a3aa21a5323b03f686c
marcosarm <marcosarm@gmail.com>
2016-11-09 10:13:33 -0200

    Load PNAD data from the files ".sas" and data files found in ibge FTP. Data Load made to work with any year.

--- carga_dados/pnad/carga_pnad.php
+++ carga_dados/pnad/carga_pnad.php
@@ -1,15 +1,44 @@
 <?php
 
 ini_set('max_execution_time', 2000); //240 seconds = 4 minutes
+echo '<b>CARGA DADOS PNAD</b> ' . $execution_time . ' secs <br>'."\n\r";
+
+$servername = "agenda2030-cluster-1.cluster-c0jcgqgfnijc.us-east-1.rds.amazonaws.com";
+$username = "pnud";
+$password = "pnud2030";
+$dbname = "agenda2030";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if (!$conn) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$conn->set_charset('utf8');
+$conn->autocommit(FALSE);
+
 
 // Calculate time
 $time_start = microtime(true);
 
-// Read Column mapping from SAS
-//$sas_file = "sas/Input PNADC_1Tri_2012 a 3Tri_2015.sas";
 $diretorio_fonte = "arquivos_fonte/PNAD";
+$ano = "2013";
+$tipo = "PES";//"DOM";
+$table = "stg_pnad_pessoa";//"stg_pnad_domicilio";//
 
-$sas_file = "$diretorio_fonte/input DOM2014.sas";
+$sql = "delete from $table where v0101 = '$ano'";
+
+if ($conn->query($sql) === TRUE) {
+    echo "delete $table";
+} else {
+    $erro = "ER001: Erro - delete $table - " . $sql;
+    $conn->rollback();
+    echo "$erro\n\r";
+    die;
+}
+
+
+
+$sas_file = "$diretorio_fonte/input$tipo$ano.sas";
 $sas_handle = fopen($sas_file, "r");
 $sas_columns = [];
 
@@ -35,7 +64,7 @@ if ($sas_handle) {
             //var_dump($tmp_columns);
 
             array_push ($sas_columns, $tmp_columns);
-        }a
+        }
     }
 
     fclose($sas_handle);
@@ -45,63 +74,40 @@ if ($sas_handle) {
 //var_dump($sas_columns);
 //die;
 
-
 $end_sas_columns = end($sas_columns);
 
 // Create sql insert header
-$table = "stg_pnad_2014_domicilio_n";
 $sql_insert_pnad_header = "INSERT INTO `" . $table . "` (";
 
 foreach ($sas_columns as $key => $column) {
     $sql_insert_pnad_header .= "`". $column[1] ."`";
 
     if ($column === $end_sas_columns) {
-        $sql_insert_pnad_header .= ") VALUES (";
+        $sql_insert_pnad_header .= ") VALUES ";
     } else {
         $sql_insert_pnad_header .= ",";
     }
 
 }
 
-echo $sql_insert_pnad_header;
+//echo $sql_insert_pnad_header;
 
 // Read data file
-$data_file = "$diretorio_fonte/DOM2014.txt";
+$data_file = "$diretorio_fonte/$tipo$ano.txt";
 $data_handle = fopen($data_file, "r");
 $data_lines = 0;
-echo "abrir arquivo $data_file \n\r";
+//echo "abrir arquivo $data_file \n\r";
 
-// Db connection
-$servername = "diagnosticopublico-cluster.cluster-czzcnh6qx1i8.us-east-1.rds.amazonaws.com";
-$username = "pnud";
-$password = "pnud2030";
-$dbname = "agenda2030";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-// turn OFF auto
-mysqli_autocommit($conn, FALSE);
-
-$i = 0;
+    $i = 0;
 if ($data_handle) {
 
     while (($line = fgets($data_handle)) !== false) {
         // Checks if is an SAS instruction line
-        $sql_insert_pnad_row = "";
+        $sql_insert_pnad_row .= "(";
         foreach ($sas_columns as $key => $column) {
 
             $sql_insert_pnad_row .= "'" . trim( substr($line, $column[0] - 1, $column[2]) ) . "'";
             $i++;
-
-            if ($i > 1000) {
-                echo $sql_insert_pnad_row;
-                die;
-            }
-            //echo $i;
 
             if ($column === $end_sas_columns) {
                 $sql_insert_pnad_row .= ")";
@@ -109,16 +115,46 @@ if ($data_handle) {
                 $sql_insert_pnad_row .= ",";
             }
         }
-        // Insert here!
-        mysqli_query($conn, $sql_insert_pnad_header . $sql_insert_pnad_row);
 
         $data_lines++;
+        echo $data_lines;
+        $sql_insert_pnad_row .= ",";
+
+
+        if ($data_lines >  10000) {
+            // Tira o ultimo caractere (virgula extra)
+            $sql_insert_pnad_row = substr($sql_insert_pnad_row, 0, -1);
+
+            if ($conn->query($sql_insert_pnad_header . $sql_insert_pnad_row) === TRUE) {
+                echo "insert $table";
+            } else {
+                $erro = "ER001: Erro - insert $table - " . $sql;
+                echo "$erro\n\r";
+                $conn->rollback();
+                die;
+            }
+
+            $data_lines = 1;
+            $sql_insert_pnad_row = "";
+        }
     }
 
+    $sql_insert_pnad_row = substr($sql_insert_pnad_row, 0, -1);
+
+    if ($conn->query($sql_insert_pnad_header . $sql_insert_pnad_row) === TRUE) {
+        echo "insert $table";
+    } else {
+        $erro = "ER001: Erro - insert $table - " . $sql;
+        echo "$erro\n\r";
+        $conn->rollback();
+        die;
+    }
+
+
     // Commit your changes
-    mysqli_commit($conn);
+    $conn->commit();
     // Close Database
-    mysqli_close($conn);
+    $conn->close();
 
     fclose($data_handle);
 
@@ -131,10 +167,4 @@ $execution_time = (microtime(true) - $time_start);
 
 // Execution time of the script
 echo '<b>Total Execution Time:</b> ' . $execution_time . ' secs <br>';
-echo '<b>Processed lines:</b> ' . $data_lines. '<br>';
-echo '<b>Sample Line:</b> <br>';
-
-echo '<pre>';
-echo $sql_insert_pnad_header . $sql_insert_pnad_row;
-echo '</pre>';
 ?>
